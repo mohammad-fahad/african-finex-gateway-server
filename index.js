da6d0b9254d1de19aa009a19cdd8681c5f570e44
Mohammad Fahad <mail.maohammadfahad@gmail.com>
2021-01-26 07:46:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7xlib.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  
+  const buy = client.db("african-finex").collection("buy");
+  const sell = client.db("african-finex").collection("sell");
+  
+  console.log("Database connected");
+
+  
+
+  app.post("/buy", (req, res) => {
+    buy.insertOne(req.body).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  
+  
+  app.post("/sell", (req, res) => {
+    sell.insertOne(req.body).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+});
